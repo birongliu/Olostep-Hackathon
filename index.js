1e82016db60794ee69bc3369069ecbeed66f2747
H-Asmaa <asmaehadar32@gmail.com>
2024-08-17 06:43:15 +0100

    Committing user authentication initial setup.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
-});
+connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.use(express.json({ extended: false }));
+
+app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
