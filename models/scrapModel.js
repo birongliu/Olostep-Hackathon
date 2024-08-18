e0dc9696e50059f8e6c4e8405e564c32532f1fe7
H-Asmaa <asmaehadar32@gmail.com>
2024-08-18 17:25:33 +0100

    Committing the data model.

---
+++ models/scrapModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const scrapedDataSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  headings: {
+    type: [String],
+  },
+  links: [
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+      href: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  paragraphs: {
+    type: [String],
+  },
+  analysisSummary: {
+    type: String,
+  },
+  insights: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ScrapedData = mongoose.model('ScrapedData', scrapedDataSchema);
+
+module.exports = ScrapedData;
