d97a0833146c1a64edd6ce9eb749dbb17d838ed5
H-Asmaa <asmaehadar32@gmail.com>
2024-08-18 19:51:39 +0100

    Committing updates to the landing page.

--- src/app/landingPage.tsx
+++ src/app/landingPage.tsx
@@ -3,6 +3,59 @@
 import { useState } from 'react';
 import { SignInButton } from '@clerk/nextjs';
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const creators = [
+  {
+    name: 'Pranitha Ramaswamy',
+    role: 'UI/UX Designer',
+    imageSrc: '/img/creator2.jpg',
+    description: 'blah blah blah',
+    socialLinks: {
+      Github: 'https://github.com/pranitha05',
+      instagram: 'https://www.instagram.com/pranitha',
+      linkedin: 'https://www.linkedin.com/in/pranitha-r-276307251/',
+      twitter: 'https://twitter.com/pranitha',
+    },
+  },
+  {
+    name: 'Minh Le',
+    role: 'Front-End Developer',
+    imageSrc: '/img/creator1.png',
+    description: 'blah blah blah',
+    socialLinks: {
+      Github: 'https://www.github.com/minh',
+      instagram: 'https://www.instagram.com/minh',
+      linkedin: 'https://www.linkedin.com/in/minh',
+      twitter: 'https://twitter.com/minh',
+    },
+  },
+  {
+    name: 'Asmaa HADAR',
+    role: 'Project manager & Back-End Developer',
+    imageSrc: '/img/creator3.png',
+    description: 'blah blah blah',
+    socialLinks: {
+      Github: 'https://github.com/H-Asmaa',
+      instagram: 'https://www.instagram.com/asmaahadar2',
+      linkedin: 'https://www.linkedin.com/in/h-asmaa',
+      twitter: 'https://x.com/HadarAsmaa',
+    },
+  },
+  {
+    name: 'Bi Rong Liu',
+    role: 'Automation & DevOps Engineer',
+    imageSrc: '/img/creator4.jpg',
+    description: 'blah blah blah',
+    socialLinks: {
+      Github: 'https://github.com/birongliu',
+      instagram: 'https://www.instagram.com/birong',
+      linkedin: 'https://www.linkedin.com/in/birong',
+      twitter: 'https://twitter.com/birong',
+    },
+  },
+];
 
 const InfoPage: React.FC = () => {
   const [formState, setFormState] = useState({
@@ -50,8 +103,6 @@ const InfoPage: React.FC = () => {
       setTimeout(() => setMessageState({ success: false, error: false }), 10000);
     }
   };
-
-
 
   return (
     <div className="relative w-full min-h-screen bg-gray-900 text-white">
@@ -109,7 +160,6 @@ const InfoPage: React.FC = () => {
               Scrapetastic
             </div>
 
-
             {/* Buttons */}
             <div className="flex justify-center items-center">
               <div className="px-4 py-3 bg-gradient-to-r from-purple-700 to-blue-700 text-white rounded-lg hover:from-purple-600 hover:to-blue-800 transition">
@@ -125,162 +175,67 @@ const InfoPage: React.FC = () => {
       {/* Description Section */}
       <section className="relative flex flex-col items-center w-full py-16 md:py-24 lg:py-32 bg-white">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center text-blue-900">
-
           What is Scrapetastic?
-
         </h2>
 
         <p className="text-center max-w-full md:max-w-2xl lg:max-w-3xl text-lg md:text-xl leading-relaxed text-gray-800 px-4">
-
           Scrapetastic is your go-to tool for transforming any webpage into valuable data with ease. Simply provide a URL, and our platform will effortlessly scrape the content,
           store it, and display it for you to explore. Whether you&apos;re a developer, researcher, or just curious, Scrapetastic makes web scraping simple and accessible.
           Stay tuned for updates as we continuously improve our platform through regular updates and enhancements.
-
         </p>
       </section>
 
-
-
-
-
       {/* Creator Section */}
       <section id="creators" className="py-32 bg-gradient-to-r from-purple-800 via-blue-800 to-purple-800 text-white">
-
         <div className="container mx-auto px-6">
-
-          <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">Who are we?</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-
-
-            {/* Creator 1 */}
-            <div className="flex flex-col items-center text-center mb-8 md:mb-10 lg:mb-12">
-
-              <div className="mb-6">
-                <Image
-                  className="w-64 h-64 object-cover rounded-md"
-                  src="/img/creator2.jpg"
-                  alt="Creator 1"
-                  width={256}
-                  height={256}
-                  loading="lazy"
-                  style={{ borderRadius: '0.375rem' }} // This is equivalent to `rounded-md`
-                />
-              </div>
-
-              <div className="mb-4">
-
-                <strong className="text-1xl md:text-2xl block mb-2">Pranitha Ramaswamy</strong>
-                <span className="text-xl block tracking-wide text-gray-400 text-opacity-70 mb-4">UI/UX Designer</span>
-                <span className="inline-block w-16 h-1 bg-gray-700"></span>
-
-              </div>
-
-              <div className="text-gray-200">
-                blah blah blah
-              </div>
+          <h2 className="text-4xl text-center font-bold mb-16">Meet the Creators</h2>
+          <div className="flex flex-wrap justify-center">
+            <div className="flex flex-wrap justify-center">
+              {creators.map((creator, index) => (
+                <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+                  <div className="bg-white p-6 rounded-lg shadow-lg text-center h-full flex flex-col">
+                    <Image
+                      src={creator.imageSrc}
+                      alt={creator.name}
+                      width={150}
+                      height={150}
+                      className="rounded-full mx-auto mb-4"
+                    />
+                    <h3 className="text-2xl font-bold text-gray-900 mb-2">{creator.name}</h3>
+                    <p className="text-gray-700 mb-4">{creator.role}</p>
+                    <p className="text-gray-600 mb-4 flex-grow">{creator.description}</p>
+                    <div className="flex justify-center space-x-4">
+                      {creator.socialLinks.Github && (
+                        <a href={creator.socialLinks.Github} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
+                          <FontAwesomeIcon icon={faGithub} size="2x" />
+                        </a>
+                      )}
+                      {creator.socialLinks.instagram && (
+                        <a href={creator.socialLinks.instagram} target="_blank" rel="noopener noreferrer" className="text-pink-600 hover:text-pink-800">
+                          <FontAwesomeIcon icon={faInstagram} size="2x" />
+                        </a>
+                      )}
+                      {creator.socialLinks.linkedin && (
+                        <a href={creator.socialLinks.linkedin} target="_blank" rel="noopener noreferrer" className="text-blue-700 hover:text-blue-900">
+                          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                        </a>
+                      )}
+                      {creator.socialLinks.twitter && (
+                        <a href={creator.socialLinks.twitter} target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600">
+                          <FontAwesomeIcon icon={faTwitter} size="2x" />
+                        </a>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-
-            {/* Creator 2 */}
-            <div className="flex flex-col items-center text-center mb-8 md:mb-10 lg:mb-12">
-
-              <div className="mb-6">
-                <Image
-                  className="w-64 h-64 object-cover rounded-md"
-                  src="/img/creator1.png"
-                  alt="Creator 2"
-                  width={256}
-                  height={256}
-                  loading="lazy"
-                  style={{ borderRadius: '0.375rem' }} // This is equivalent to `rounded-md`
-                />
-              </div>
-
-              <div className="mb-4">
-
-
-                <strong className="text-1xl md:text-2xl block mb-2">Minh Le</strong>
-                <span className="text-xl block tracking-wide text-gray-400 text-opacity-70 mb-4">Front-End Developer</span>
-                <span className="inline-block w-16 h-1 bg-gray-700"></span>
-
-              </div>
-
-              <div className="text-gray-200">
-                blah blah blah
-
-              </div>
-
-            </div>
-
-            {/* Creator 3 */}
-            <div className="flex flex-col items-center text-center mb-8 md:mb-10 lg:mb-12">
-
-              <div className="mb-6">
-
-                <Image
-                  className="w-64 h-64 object-cover rounded-md"
-                  src="/img/creator3.png"
-                  alt="Creator 3"
-                  width={256}
-                  height={256}
-                  loading="lazy"
-                  style={{ borderRadius: '0.375rem' }}
-                />
-              </div>
-
-              <div className="mb-4">
-
-                <strong className="text-1xl md:text-2xl block mb-2">Asmaa HADAR</strong>
-                <span className="text-xl block tracking-wide text-gray-400 text-opacity-70 mb-4">Project manager & Back-End Developer</span>
-                <span className="inline-block w-16 h-1 bg-gray-700"></span>
-
-              </div>
-
-              <div className="text-gray-200">
-
-                blah blah blah
-
-              </div>
-            </div>
-
-            {/* Creator 4 */}
-            <div className="flex flex-col items-center text-center mb-8 md:mb-10 lg:mb-12">
-
-              <div className="mb-6">
-
-                <Image
-                  className="w-64 h-64 object-cover rounded-md"
-                  src="/img/creator4.jpg"
-                  alt="Creator 4"
-                  width={256}
-                  height={256}
-                  loading="lazy"
-                  style={{ borderRadius: '0.375rem' }} // This is equivalent to `rounded-md`
-                />
-
-              </div>
-
-              <div className="mb-4">
-
-                <strong className="text-1xl md:text-2xl block mb-2">Bi Rong Liu</strong>
-                <span className="text-xl block tracking-wide text-gray-400 text-opacity-70 mb-4">Automation & DevOps Engineer</span>
-                <span className="inline-block w-16 h-1 bg-gray-700"></span>
-
-              </div>
-              <div className="text-gray-200">
-
-                blah blah blah
-              </div>
-
-            </div>
-
           </div>
-
         </div>
-
       </section>
 
-      {/* Contact Section */}
-      <section className="py-16 bg-gray-100">
+      {/* Contact Form Section */}
+      <section id="contact" className="py-16 bg-gray-100">
         <div className="container mx-auto px-4">
           <div className="text-center mb-12">
             <h2 className="text-4xl font-bold text-blue-900 mb-4">Contact Us Here</h2>
